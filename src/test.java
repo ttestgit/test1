c41681dd2168f1f17451ef0435f587fdf8fdfef1
ttestgit <ttestgit@yandex.ru>
2021-08-12 17:15:00 +0500

    date format test

--- src/test.java
+++ src/test.java
@@ -1,3 +1,8 @@
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -5,13 +10,17 @@ import java.util.regex.Pattern;
 public class test {
     public static void main(String[] args) throws  Exception
     {
-        Pattern pattern = Pattern.compile("\\d+");
+        /*Pattern pattern = Pattern.compile("\\d+");
         Matcher matcher = pattern.matcher("asdasd 2000 dsad as 3500 asfweroiu 34634 qweqw 3400");
         int sum = 0;
         while (matcher.find())
         {
             sum += Integer.parseInt(matcher.group());
         }
-        System.out.println(sum);
+        System.out.println(sum);*/
+
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(format.format(date));
     }
 }
