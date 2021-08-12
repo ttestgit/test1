8a60cb1f69eab5b2052f95842fd49ed0a0869c98
ttestgit <ttestgit@yandex.ru>
2021-08-12 17:12:36 +0500

    regex test

--- src/test.java
+++ src/test.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class test {
     public static void main(String[] args) throws  Exception
     {
-        String str = "123123,123";
-        double d = Double.parseDouble(str);
-        Scanner scn = new Scanner(System.in);
-        System.out.println("asdasdwe");
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher("asdasd 2000 dsad as 3500 asfweroiu 34634 qweqw 3400");
+        int sum = 0;
+        while (matcher.find())
+        {
+            sum += Integer.parseInt(matcher.group());
+        }
+        System.out.println(sum);
     }
 }
